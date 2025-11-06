6a18723357b5e70f24b2812d86083be90bb7499b
Mihai Dogaru <dogarumihai2537@gmail.com>
2025-11-06 18:28:54 +0200

    Change selected rating on thanks screen

--- js/main.js
+++ js/main.js
@@ -5,18 +5,36 @@ console.log("Hello world");
 const btnSubmitEl = document.getElementById("btnSubmit");
 const formEl = document.getElementById("formRating");
 const sectionThanksEl = document.querySelector(".card--thanks");
+const radioEls = document.querySelectorAll("input[type='radio']");
+const selectionEl = document.querySelector(".selection-text > span");
+
+console.log(selectionEl);
+
+let uiState = { submitted: false };
 
 initBackNav(handleBackPressed);
 
+function render(state) {
+  if (state.submitted) {
+    showThanks();
+    return;
+  }
+
+  showHome();
+}
+
 btnSubmitEl.addEventListener("click", (ev) => {
   ev.preventDefault();
+  if (!validateInput()) return;
+
   history.pushState({ test: 123 }, "", "");
   showThanks();
 
   console.log("Clicked");
+  console.log(getSelectedRating());
 });
 
-function initHomePage() {
+function showHome() {
   formEl.classList.remove("hidden-left");
   formEl.setAttribute("aria-hidden", "false");
 
@@ -25,7 +43,7 @@ function initHomePage() {
 }
 
 function handleBackPressed() {
-  initHomePage();
+  showHome();
 }
 
 function showThanks() {
@@ -34,4 +52,24 @@ function showThanks() {
 
   sectionThanksEl.classList.remove("hidden-right");
   sectionThanksEl.setAttribute("aria-hidden", "false");
+
+  let selectedRating = getSelectedRating();
+  selectionEl.textContent = selectedRating;
+}
+
+function validateInput() {
+  return Array.from(radioEls).some((radio) => radio.checked);
+}
+
+function getSelectedRating() {
+  let radio = Array.from(radioEls).find((radio) => radio.checked);
+  if (!radio) return "0";
+
+  let idItems = radio.id.split("-");
+  if (idItems.length < 2) {
+    console.log("Something wrong with the radio id");
+    return "0";
+  }
+
+  return idItems[1];
 }
